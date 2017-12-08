2e3fe7be87aa7888f3255ca69237ed388135ea34
杨晓杰 <1127129489@qq.com>
2017-12-09 01:21:14 +0800

    d

--- app/Http/Controllers/Admin/TestController.php
+++ app/Http/Controllers/Admin/TestController.php
@@ -49,7 +49,7 @@ class TestController extends Controller
         $response = $c->sdkExecute($request);
         //$response = urldecode($response);
 //htmlspecialchars是为了输出到页面时防止被浏览器将关键参数html转义，实际打印到日志以及http传输不会有这个问题
-        echo htmlspecialchars($response);//就是orderString 可以直接给客户端请求，无需再做处理。
+        echo $response;//就是orderString 可以直接给客户端请求，无需再做处理。
 
     }
 
