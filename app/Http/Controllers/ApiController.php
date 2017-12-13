044ef6511dad42479481803b5b88721774537ebc
杨晓杰 <1127129489@qq.com>
2017-12-13 22:33:30 +0800

    debug

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -313,7 +313,7 @@ class ApiController extends Controller
         file_put_contents(storage_path().'/sdfdsfsdf2.txt',$_POST['trade_status']);
 
         if($verify_res == 'true' && $_POST['trade_status'] == 'TRADE_SUCCESS'){
-            file_put_contents(storage_path().'/sdfdsfsdf.txt',$_POST['out_trade_no']);
+            file_put_contents(storage_path().'/12344.txt',$_POST['out_trade_no']);
             //验签完毕
             $order_id = $_POST['out_trade_no'];
             $price = $_POST['price'];
