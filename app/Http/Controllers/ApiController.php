8997d248a20dd02d1d6e43fc873b8caa2a07079a
杨晓杰 <1127129489@qq.com>
2017-12-13 21:23:00 +0800

    debug

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -831,7 +831,7 @@ class ApiController extends Controller
             return response() -> json(['status'=>'error']);
         }
 
-
+        $price = 0.01;
         //生成订单编号
         $out_trade_no = date('Ymd') . str_pad(mt_rand(1, 99999), 5, '0', STR_PAD_LEFT);
         //var_dump('http://m.jhqck.com/al/order/sign?total_fee='.$price.'&out_trade_no='.$out_trade_no);exit;
@@ -869,7 +869,7 @@ class ApiController extends Controller
             'product_code' => "QUICK_MSECURITY_PAY"
         ];
         $bizcontent = json_encode($bizcontent);
-        $request2->setNotifyUrl("http://jc.xyzxzy.cn/public/alipay");
+        $request2->setNotifyUrl("http://jc.xyzxzy.cn/api/public/alipay");
         $request2->setBizContent($bizcontent);
         //这里和普通的接口调用不同，使用的是sdkExecute
         $sign = $c->sdkExecute($request2);
